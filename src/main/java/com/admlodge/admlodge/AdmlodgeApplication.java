c53aabcc82bc232dad3be0fe0808dbf640244a41
gjwenner <gjwenner@gmail.com>
2023-10-03 23:32:38 -0300

    Creacion Primeras Entidades

---
+++ src/main/java/com/admlodge/admlodge/AdmlodgeApplication.java
@@ -0,0 +1,13 @@
+package com.admlodge.admlodge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmlodgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmlodgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admlodge/admlodge/AdmlodgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admlodge.admlodge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmlodgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
