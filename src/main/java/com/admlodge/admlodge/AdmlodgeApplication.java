80ad0837280a55776cef7488e6dbefa8ecd635df
gjwenner <gjwenner@gmail.com>
2023-10-18 00:08:50 -0300

    Creacion Primeras Entidades

--- src/main/java/com/admlodge/admlodge/AdmlodgeApplication.java
+++ src/main/java/com/admlodge/admlodge/AdmlodgeApplication.java
@@ -1,13 +1,17 @@
 package com.admlodge.admlodge;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdmlodgeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdmlodgeApplication.class, args);
-	}
-
+    //me faltaba este archivo
+    @Bean
+    public CommandLineRunner initData() {
+        return (args) -> {
+        };
+    }
 }
